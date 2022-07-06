c99875b6eec15638260c4d9617654f9ddbf11004
Raul <you@example.com>
2022-07-06 13:33:25 +0200

    setup token

--- src/route/userRoute.ts
+++ src/route/userRoute.ts
@@ -4,7 +4,7 @@ import auth from '../middleware/authHandler';
 import jwt from '../middleware/jwtHandler';
 const router = Router();
 
-router.post('/user',userController.saveUser);
+router.post('/user',auth.encryptPassword,userController.saveUser);
 router.post('/user/login',auth.validateUser,userController.login);
 router.get('/user/all',jwt.validateToken,userController.getAllUsers);
 export default router;
