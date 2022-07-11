9e59166cdd5c675adc365faeb0716694000b0c19
Raul <you@example.com>
2022-07-11 15:42:13 +0200

    errors setup

--- src/route/userRoute.ts
+++ src/route/userRoute.ts
@@ -1,7 +1,7 @@
 import Router from 'express';
 import {userController} from '../controller/userController';
 import auth from '../middleware/authentication/authHandler';
-import jwt from '../middleware/jwtHandler';
+import jwt from '../middleware/authentication/jwtHandler';
 
 const {saveUser,getAllUsers} = userController;
 const {encryptPassword} = auth;
